d2429d1da54c72efee9b82ecc8853968255ee65a
BenWirus <benthewirus@gmail.com>
2020-12-14 23:09:50 -0500

    updated to search more fields

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ cd mccarthy
 python3 -m pip install -r requirements.txt
 ```
 
-Download the SQLite db from [here](https://gitlab.com/shanghai-ccp-member-db/shanghai-ccp-member-db/-/raw/master/shanghai-ccp-member.db) and copy it to the `mccarthy` project folder and name it `ccp.db`.
+Download the SQLite db
+from [here](https://git.maga.host/BenWirus/ChiomDB/-/raw/master/ccp_zh.db) and
+copy it to the `mccarthy` project folder and name it `ccp.db`.
 
 ## Docker Install
 
@@ -25,7 +27,7 @@ apt update
 apt install python3 python3-pip sqlite3 wget git
 git clone https://github.com/BenWirus/McCarthy.git mccarthy
 cd mccarthy
-wget -O ccp.db https://gitlab.com/shanghai-ccp-member-db/shanghai-ccp-member-db/-/raw/master/shanghai-ccp-member.db
+wget -O ccp.db https://git.maga.host/BenWirus/ChiomDB/-/raw/master/ccp_zh.db
 python3 -m pip install -r requirements.txt
 ```
 
@@ -34,15 +36,15 @@ python3 -m pip install -r requirements.txt
 *nix:
 
 ```bash
-./mccarthy.py -n <name>
-./mccarthy.py --name <name>
+./mccarthy.py -n <name> -h <home> -o <organization> -a <address> -m <mobile_phone_number> -p <phone_number>
+./mccarthy.py --name <name> --home <home> --org <organization> --address <address> --mobile <mobile_phone_number> --phone <phone_number>
 ```
 
 win:
 
 ```bash
-python3 mccarthy.py -n <name>
-python3 mccarthy.py --name <name>
+python3 mccarthy.py -n <name> -h <home> -o <organization> -a <address> -m <mobile_phone_number> -p <phone_number>
+python3 mccarthy.py --name <name> --home <home> --org <organization> --address <address> --mobile <mobile_phone_number> --phone <phone_number>
 ```
 
 ## Results
@@ -55,10 +57,16 @@ Translated JSON results will be dumped to stdout, and a timestamped JSON file wi
 * Multiple result sets
     * Searches the name as a Chinese translated string
     * Searches the name as it was entered
+* Search:
+    * Name
+    * Hometown
+    * Organization
+    * Address
+    * Mobile Number
+    * Phone Number
 
 ## Needed Improvements
 
 * Something other than SQLite to store the searchable data maybe Elasticsearch?
 * Translation result caching.
-* Search more than the name. E.G. address, phone, hometown, CCP ID Number, etc.
 * Support other languages besides English.
--- mccarthy.py
+++ mccarthy.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 """
-Mccarthyism is back in style. Make these bitches rout.
+McCarthyism is back in style. Make these bitches rout.
 """
 
 import sys
@@ -46,19 +46,76 @@ def print_help(code: int = 0):
     :param code: int
     :return: void
     """
-    print(sys.argv[0] + ' -n <name>')
-    print(sys.argv[0] + ' --name <name>')
+    print(
+        sys.argv[0] + ' -n <name> -h <home> -o <organization> -a <address> -m <mobile_phone_number> -p <phone_number>'
+    )
+    print(
+        sys.argv[0] +
+        ' --name <name> --home <home> --org <organization> --address <address> --mobile <mobile_phone_number> --phone <phone_number>'
+    )
     sys.exit(code)
 
 
-def run(name):
+def run_translations(rows):
+    results = []
+
+    for row in rows:
+        try:
+            results.append(
+                pool.map(do_translate_zh_en, [
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6],
+                    row[7],
+                    row[8],
+                    row[9],
+                    row[10]
+                ]))
+        except Exception as e:
+            raise e
+
+    return results
+
+
+def search_db(term, zh_term, type):
     # Open the DB
     conn = sqlite3.connect('ccp.db')
     cursor = conn.cursor()
-    # Translate the name to Chinese
-    zh_name = do_translate_auto_zh(name)
     # The search query @todo implement FTS with ranks instead of ugly like search
-    name_query = """SELECT *  FROM member WHERE member.name LIKE '%'||?||'%';"""
+    query = "SELECT *  FROM member WHERE member." + type + " LIKE '%'||?||'%';"
+
+    if term != zh_term:
+        # Search the DB for the translated chinese name
+        print('Searching for ' + type + ' ' + zh_term + '(' + term + ')...')
+        cursor.execute(query, [zh_term])
+        zh_search_rows = cursor.fetchall()
+    else:
+        print(
+            'Skipping translated search. The translated term is the same as the original term. (%s - %s)'
+            % (term, zh_term)
+        )
+        zh_search_rows = []
+
+    # Search the DB for the untranslated name
+    print('Searching for ' + type + ' ' + term + '...')
+    cursor.execute(query, [term])
+    untrans_search_rows = cursor.fetchall()
+
+    # Close the DB
+    cursor.close()
+    conn.close()
+
+    print('Found %s Chinese translated %s results' % (str(len(zh_search_rows)), type))
+    print('Found %s untranslated %s results' % (str(len(untrans_search_rows)), type))
+    print('Translating results...')
+    return [run_translations(zh_search_rows), run_translations(untrans_search_rows)]
+
+
+def run(name, home, org, addr, mobile, phone):
     # Results
     results = {
         'keys': [
@@ -74,102 +131,108 @@ def run(name):
             'phone_num',
             'education'
         ],
-        'chinese_translated': [],
-        'untranslated': []
+        'chinese_translated': {},
+        'untranslated': {}
     }
 
-    # Search the DB for the translated chinese name
-    print('Searching for name ' + zh_name + '(' + name + ')...')
-    cursor.execute(name_query, [zh_name])
-    zh_search_rows = cursor.fetchall()
+    ###
+    # Initial translations to search as Chinese
+    ###
+    # Translate the name to Chinese
+    zh_name = do_translate_auto_zh(name) if name else False
+    # Translate the home to Chinese
+    zh_home = do_translate_auto_zh(home) if home else False
+    # Translate the org to Chinese
+    zh_org = do_translate_auto_zh(org) if org else False
+    # Translate the address to Chinese
+    zh_addr = do_translate_auto_zh(addr) if addr else False
+    # Translate the mobile to Chinese
+    zh_mobile = do_translate_auto_zh(mobile) if mobile else False
+    # Translate the phone to Chinese
+    zh_phone = do_translate_auto_zh(phone) if phone else False
 
-    # Search the DB for the untranslated name
-    print('Searching for name ' + name + '...')
-    cursor.execute(name_query, [name])
-    untrans_search_rows = cursor.fetchall()
+    if name:
+        r = search_db(name, zh_name, 'name')
+        results['chinese_translated']['name'] = r[0]
+        results['untranslated']['name'] = r[1]
 
-    # Close the DB
-    cursor.close()
-    conn.close()
+    if home:
+        r = search_db(home, zh_home, 'hometown')
+        results['chinese_translated']['hometown'] = r[0]
+        results['untranslated']['hometown'] = r[1]
 
-    print('Found %s Chinese translated search results' % str(len(zh_search_rows)))
-    print('Found %s untranslated search results' % str(len(untrans_search_rows)))
+    if org:
+        r = search_db(org, zh_org, 'organization')
+        results['chinese_translated']['organization'] = r[0]
+        results['untranslated']['organization'] = r[1]
 
-    # Translate the results to english
-    print('Translating results...')
-    for row in zh_search_rows:
-        try:
-            results['chinese_translated'].append(
-                pool.map(do_translate_zh_en, [
-                    row[0],
-                    row[1],
-                    row[2],
-                    row[3],
-                    row[4],
-                    row[5],
-                    row[6],
-                    row[7],
-                    row[8],
-                    row[9],
-                    row[10]
-                ]))
-        except Exception as e:
-            raise e
+    if addr:
+        r = search_db(addr, zh_addr, 'address')
+        results['chinese_translated']['address'] = r[0]
+        results['untranslated']['address'] = r[1]
 
-    # Translate the results to english
-    for row in untrans_search_rows:
-        try:
-            results['untranslated'].append(
-                pool.map(do_translate_zh_en, [
-                    row[0],
-                    row[1],
-                    row[2],
-                    row[3],
-                    row[4],
-                    row[5],
-                    row[6],
-                    row[7],
-                    row[8],
-                    row[9],
-                    row[10]
-                ]))
-        except Exception as e:
-            raise e
+    if mobile:
+        r = search_db(mobile, zh_mobile, 'mobile_num')
+        results['chinese_translated']['mobile_num'] = r[0]
+        results['untranslated']['mobile_num'] = r[1]
+
+    if phone:
+        r = search_db(phone, zh_phone, 'phone_num')
+        results['chinese_translated']['phone_num'] = r[0]
+        results['untranslated']['phone_num'] = r[1]
 
     # Run threads
     pool.close()
     pool.join()
 
     # Dump json to file
-    with open('out/' + name + '-' + str(time.time() * 1000.0) + '.json', 'w') as outfile:
+    with open('out/' + str(time.time() * 1000.0) + '.json', 'w') as outfile:
         json.dump(results, outfile)
     # Dump json to stdout
     print(json.dumps(results))
 
 
 def main(argv):
-    """
-    Parses arguments from the commandline
-    :param argv: list
-    :return: void
-    """
     name = False
+    home = False
+    org = False
+    addr = False
+    mobile = False
+    phone = False
 
     try:
-        opts, args = getopt.getopt(argv, "h:n:", ["name="])
+        opts, args = getopt.getopt(argv, "Hn:h:o:a:m:p:", [
+            "help",
+            "name=",
+            "home=",
+            "org=",
+            "address=",
+            "mobile=",
+            "phone="
+        ])
     except getopt.GetoptError:
         print_help(1)
 
     for opt, arg in opts:
-        if opt == '-h':
+        if opt in ('-H', '--help'):
             print_help()
         elif opt in ("-n", "--name"):
             name = arg
+        elif opt in ("-H", "--home"):
+            home = arg
+        elif opt in ("-o", "--org"):
+            org = arg
+        elif opt in ("-a", "--address"):
+            addr = arg
+        elif opt in ("-m", "--mobile"):
+            mobile = arg
+        elif opt in ("-p", "--phone"):
+            phone = arg
 
-    if not name:
+    if name or home or org or addr or mobile or phone:
+        run(name, home, org, addr, mobile, phone)
+    else:
         print_help(1)
-
-    run(name)
 
 
 if __name__ == "__main__":
